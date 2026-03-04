d0a013c6ff090b658fc09436f3e21fdf6e9d031c
verdi <zuev.jurij@@gmail.com>
2026-03-04 19:47:58 +0200

    Initial Commit

---
+++ Fitify/FitifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitifyApp.swift
+//  Fitify
+//
+//  Created by Юрій on 3/4/26.
+//
+
+import SwiftUI
+
+@main
+struct FitifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitifyTests/FitifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitifyTests.swift
+//  FitifyTests
+//
+//  Created by Юрій on 3/4/26.
+//
+
+import Testing
+@testable import Fitify
+
+struct FitifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
